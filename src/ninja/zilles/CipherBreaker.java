f3541020c6b54935ca3e2010f2832766d6ffdd77
Craig Zilles <zilles@illinois.edu>
2016-06-26 09:32:14 -0500

    Initial commit

---
+++ src/ninja/zilles/CipherBreaker.java
@@ -0,0 +1,17 @@
+package ninja.zilles;
+
+/**
+ * See CipherBreaker.txt for instructions.
+ * TODO: add your netid to the line below
+ * 
+ * @author put-your-netid-here
+ */
+public class CipherBreaker {
+
+	public static void main(String[] args) {
+		TextIO.putln("Text?");
+		String line = TextIO.getln();
+		TextIO.putln(line);
+
+	}
+}
